0518586b70486139b2e7911b1320575370324b7c
Martin Streicher <martin.streicher@gmail.com>
2013-02-12 16:56:17 -0500

    Refer to browserdetect in ./lib

--- jquery-autotagging/jquery.autotagging.js
+++ jquery-autotagging/jquery.autotagging.js
@@ -2,7 +2,7 @@
 (function() {
   var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-  define(['jquery', 'lib/browserdetect', 'jquery-cookie-rjs'], function($, browserdetect) {
+  define(['jquery', './lib/browserdetect', 'jquery-cookie-rjs'], function($, browserdetect) {
     var WH;
     return WH = (function() {
 
