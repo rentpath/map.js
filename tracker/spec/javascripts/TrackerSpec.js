4fd8bb6c5bb65b696cab450b9994d9ac59710b60
Akil Brooks <akil.brooks@gmail.com>
2013-04-10 15:00:02 -0400

    [AB] [47750459] Write first test

--- tracker/spec/javascripts/TrackerSpec.js
+++ tracker/spec/javascripts/TrackerSpec.js
@@ -1,13 +1,35 @@
 describe("Tracker Suite", function() {
+  var tracker, key;
+
+  var checkStorage = function(field) {
+    var item = JSON.parse(localStorage.getItem(key));
+    return item[field] || null;
+  };
 
   beforeEach(function() {
+    var ready = false;
+    var key = "/apartments/Alaska/Yakutat/";
 
+    localStorage.clear();
+
+    require(['../../tracker'], function(localTracker) {
+      tracker = localTracker;
+
+      tracker.key = function() { key };
+
+      ready = true;
+    });
+
+    waitsFor(function(){
+      return ready;
+    });
   });
 
   describe("Public Static Methods", function() {
-    describe("#track",  function() {
-      it('', function () {
-
+    describe("#track", function() {
+      it('should increment the count', function () {
+        tracker.track();
+        expect(checkStorage('count')).toEqual('1');
       });
     });
   });
