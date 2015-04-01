0836a46622010ee3ebde42aa9e2f673919619bb7
warren vosper <straydogsw@gmail.com>
2015-04-01 13:43:38 -0600

    [WV] Updated dist/

--- dist/components/mixins/distance_conversion.js
+++ dist/components/mixins/distance_conversion.js
@@ -5,25 +5,22 @@ define([], function() {
     meterToKilometer = 1000;
     kmRatio = 0.62137;
     this._metersToKilometers = function(meters) {
-      if (!this._distanceParamDefined(meters)) {
+      if (!meters) {
         return 0;
       }
       return (meters / meterToKilometer) * kmRatio;
     };
     this._milesToMeters = function(miles) {
-      if (!this._distanceParamDefined(miles)) {
+      if (!miles) {
         return 0;
       }
       return (miles / kmRatio) * meterToKilometer;
     };
     this._metersToMiles = function(meters) {
-      if (!this._distanceParamDefined(meters)) {
+      if (!meters) {
         return 0;
       }
       return (meters / meterToKilometer) * kmRatio;
-    };
-    this._distanceParamDefined = function(p) {
-      return typeof p !== 'undefined' && p && p !== '';
     };
     this.convertMilesToMeters = function(miles) {
       return this._milesToMeters(miles);
